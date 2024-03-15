6923e90c412a1fe1f4255a90e20201a30abd8ba4
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-03-15 12:14:54 -0300

    Update aula1503.js

--- exer/aula1503.js
+++ exer/aula1503.js
@@ -1,1 +1,10 @@
+let x = 10;
+let y = 10;
+
+console.log(!(x == y));
+
+let nome = "yuri"; 
+let idade=16
+let mensagem = idade>=18 ? `Olá, ${nome}. Você é um adulto.`:`Olá,${nome}. Você é menor de idade.`;
+console.log(mensagem);
 
