aef02fa96df7a2304d262c58e657bdcda1a1bf07
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-03-15 12:32:57 -0300

    Update trab1.js

--- trab/trab1.js
+++ trab/trab1.js
@@ -1,1 +1,14 @@
+let x = number(prompt("digite um número inteiro: "));
+//verificar se é positivo 
+if(x>0){
+console.log("O x é positivo");
+}
 
+//verificar se é negativo
+else if (x<0){
+    console.log("O x é negativo");
+}
+
+else{
+    console.log("O x é zero");
+}
