4a9b67044abe1a9c46fee12424008a9709a593cd
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-04-05 11:20:00 -0300

    Update list2.js

--- pŕimeiro_TRI/lista_prova/list2.js
+++ pŕimeiro_TRI/lista_prova/list2.js
@@ -1,1 +1,11 @@
-
+let numero = parseInt(prompt("Digite um número"));
+if (numero >= 100) {
+    console.log("O número é maior ou igual a 100");
+} else {
+    let dezena = parseInt(numero / 10);
+    console.log("Dezena:", dezena);
+    let unidade = numero % 10;
+    console.log("Unidade:", unidade);
+    let soma = dezena + unidade;
+    console.log("A soma dos dígitos é:", soma);
+}
