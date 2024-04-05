e7f612d926f7f4083ba5bb9fcf511b4ff0a1c7ae
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-04-05 11:21:29 -0300

    Update list4.js

--- pŕimeiro_TRI/lista_prova/list4.js
+++ pŕimeiro_TRI/lista_prova/list4.js
@@ -1,1 +1,5 @@
-
+const nota1 = parseFloat(prompt("Nota 1: "));
+const nota2 = parseFloat(prompt("Nota 2: "));
+const nota3 = parseFloat(prompt("Nota 3: "));
+const nota4 = parseFloat(prompt("Nota 4: "));
+console.log("A média é:", (nota1 + nota2 + nota3 + nota4) / 4);
