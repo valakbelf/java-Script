fbe62c87ded01365ee0112f4defcb7d68f9056e4
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-04-05 11:32:18 -0300

    Update list7.js

--- pŕimeiro_TRI/lista_prova/list7.js
+++ pŕimeiro_TRI/lista_prova/list7.js
@@ -1,8 +1,10 @@
-function somarnumero(num1, num2, num3) {
-    console.log("A soma é:", num1 + num2 + num3);
-}
+const numero1 = parseFloat(prompt("Número 1: "));
+const numero2 = parseFloat(prompt("Número 2: "));
+const numero3 = parseFloat(prompt("Número 3: "));
 
-const num1 = parseFloat(prompt("Número 1: "));
-const num2 = parseFloat(prompt("Número 2: "));
-const num3 = parseFloat(prompt("Número 3: "));
-somarnumero(num1, num2, num3);
+let maior = numero1;
+
+if (numero2 > maior) maior = numero2;
+if (numero3 > maior) maior = numero3;
+
+console.log("O maior é:", maior);m3);
