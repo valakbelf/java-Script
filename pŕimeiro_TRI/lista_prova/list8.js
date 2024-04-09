402edab2dfbce7c50546948d95c96e825757ca32
valakbelf <161715420+valakbelf@users.noreply.github.com>
2024-04-09 11:58:09 -0300

    Update list8.js

--- pŕimeiro_TRI/lista_prova/list8.js
+++ pŕimeiro_TRI/lista_prova/list8.js
@@ -1,1 +1,17 @@
+let lado1 = parseFloat(prompt('Digite o primeiro lado: '));
+let lado2 = parseFloat(prompt('Digite o segundo lado: '));
+let lado3 = parseFloat(prompt('Digite o terceiro lado: '));
 
+if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0) {
+    console.log('Os valores dos lados devem ser maiores que zero.');
+} else if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1) {
+    if (lado1 === lado2 && lado1 === lado3) {
+        console.log('Os valores podem formar um triângulo Equilátero.');
+    } else if (lado1 === lado2 || lado1 === lado3 || lado2 === lado3) {
+        console.log('Os valores podem formar um triângulo Isósceles.');
+    } else {
+        console.log('Os valores podem formar um triângulo Escaleno.');
+    }
+} else {
+    console.log('Os valores não podem formar um triângulo.');
+}
